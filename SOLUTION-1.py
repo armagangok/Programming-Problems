4abb5a7ec4acf5d0902ad2d80f697b0a69d13199
Armagan <unknowncosmicdot@Armagans-MacBook-Air.local>
2021-06-21 13:09:22 +0300

    commit

--- SOLUTION-1.py
+++ SOLUTION-1.py
@@ -1,13 +1,13 @@
 store = []
+
 for number1 in range(999, 100, -1):
     for number2 in range(999, 100, -1):
-        str_number = str(number1*number2)[::-1]
-
-        if str_number == str(number1*number2):
-            store.append(number1*number2)
+        number = number1*number2
+        str_number = str(number)[::-1]
+        if str_number == str(number):
+            store.append(number1)
         else:
             pass
 
 store.sort()
-
 print(store[len(store)-1])
